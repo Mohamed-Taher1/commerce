504613916e6c9145c760b94820e4539ba42aa54b
Mohamed-Taher1 <mohamedtahermail@gmail.com>
2023-05-29 20:18:05 +0300

    home titles added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:commerce/constants.dart';
 import 'package:commerce/view/provider/home_provider.dart';
+import 'package:commerce/view/screens/home.dart';
 import 'package:commerce/view/screens/prev_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: PrevItem(),
+      home: HomePage(),
     );
   }
 }
