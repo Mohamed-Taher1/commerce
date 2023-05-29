1ee0c8740beec45464a07a78c70719fc04bb99d3
Sameh-Brakat <sameh13652@gmail.com>
2023-05-29 21:17:33 +0300

    change

--- lib/view/screens/prev_item.dart
+++ lib/view/screens/prev_item.dart
@@ -30,76 +30,109 @@ class PrevItem extends StatelessWidget {
               )),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-                // top: 15,
-                // left: screenWidth * .03,
-                // right: screenWidth * .03,
-                ),
-            child: Container(
-              height: screenHeight * 0.25,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 15,
+          left: screenWidth * .03,
+          right: screenWidth * .03,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: screenHeight * 0.3,
+              // padding: EdgeInsets.symmetric(horizontal: screenWidth *0.15, vertical: ),
               width: screenWidth,
               decoration: BoxDecoration(
                   color: const Color(0xFF034066),
                   borderRadius: BorderRadius.circular(13)),
-              child: Image.asset(
-                "assets/images/5-2-fruit-free-png-image.png",
-                height: screenHeight * 0.0001,
-                width: 4,
-                scale: 10,
-                fit: BoxFit.cover,
+              child: FractionallySizedBox(
+                heightFactor: 0.75,
+                // widthFactor: 0.75,
+                child: Image.asset(
+                  "assets/images/5-2-fruit-free-png-image.png",
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: screenHeight * .26,
-            child: ListView.builder(
-              padding: EdgeInsets.only(
-                left: screenWidth * .032,
-              ),
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    top: 25,
-                    left: screenWidth * .049,
-                    right: screenWidth * .049,
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Color(0xFF034066),
-                        borderRadius: BorderRadius.circular(13)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(13),
+            SizedBox(
+              height: screenHeight * .1,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                      right: screenWidth * .03,
+                    ),
+                    child: Container(
+                      width: screenWidth * 0.3,
+                      decoration: BoxDecoration(
+                          color: const Color(0xFF034066),
+                          borderRadius: BorderRadius.circular(13)),
+                      child: FractionallySizedBox(
+                        heightFactor: 0.75,
+                        // widthFactor: 0.75,
                         child: Image.asset(
-                          "assets/images/pexels-angele-j-128402 (1).jpg",
-                          fit: BoxFit.cover,
+                          "assets/images/5-2-fruit-free-png-image.png",
+                          fit: BoxFit.contain,
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Text('Fruit'),
-          Row(
-            children: [
-              Text('Alphonso mango'),
-              Row(
-                children: [
-                  Text('\$32.06'),
-                  Text('34.26'),
-                ],
+                  );
+                },
               ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.01,
+            ),
+            Text('Fruit' , style: mainFont.copyWith(color: limonColor ,fontSize: 20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Alphonso mango', style: mainFont.copyWith(color: Colors.white ,fontSize: 25)),
+                Row(
+                  children: [
+                    Text('\$32.06', style: mainFont.copyWith(color: limonColor ,fontSize: 25)),
+                    Text(' 34.26', style: mainFont.copyWith(color: Colors.grey ,fontSize: 17 ,)),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.01,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.star , color: Colors.orange,size: 30,),
+                    Text(' 4.5 ', style: mainFont.copyWith(color: Colors.white ,fontSize: 20)),
+                    Icon(Icons.chat , color: limonColor,size: 30,),
+                    Text(' 63 Fleviews ' , style: mainFont.copyWith(color: Colors.grey ,fontSize: 18)),
+                  ],
+                ),
+                Container(
+                  width: screenWidth * 0.29,
+                  height: screenHeight * 0.06,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF034066),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(Icons.plus_one, color: Colors.white,)),
+                      Text('1'),
+                      IconButton(color: Colors.white,onPressed: (){}, icon: Icon(Icons.exposure_minus_1 , color: Colors.white,)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
